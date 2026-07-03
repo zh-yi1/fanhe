2973ad0a877356a4e5efa0bff6dbd31f0136126f
正心 <13217265055@163.com>
2026-07-03 14:29:45 +0800

    配置修改

--- projects/watch466/config.h
+++ projects/watch466/config.h
@@ -94,7 +94,7 @@
 #define FLASH_CODE_BASE_SIZE            0x1C0000                                            //代码区(须<=FLASH_UI_BASE-0x2000)
 #define FLASH_UI_BASE                   0x200000                                            //UI资源起始地址(最小值为FLASH_CODE_SIZE)
 #define FLASH_UI_SIZE                   0x150000                                            //UI资源大小(ui.bin的大小)
-#define FLASH_PKG_START                 0x300000                                            //升级压缩包存放起始地址
+#define FLASH_PKG_START                 0x350000                                            //升级压缩包存放起始地址
 #define FLASH_PKG_SIZE                  0x060000                                            //升级压缩包大小
 #define FLASH_DISK_START                FLASH_PKG_START                                     //FLASH DISK 功能与OTA升级复用
 #define FLASH_DISK_LEN                  FLASH_PKG_SIZE                                      //FLASH DISK 功能与OTA升级复用, 0为关闭此功能
