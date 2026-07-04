c1902c6180ad21a533bcdd04e21fe61ea87864f1
正心 <13217265055@163.com>
2026-07-04 12:56:53 +0800

    模式界面优化

--- projects/watch466/functions/func_new_mode.c
+++ projects/watch466/functions/func_new_mode.c
@@ -42,7 +42,7 @@ extern u8 g_new_heat_proto_mode;
 #define NEW_MODE_STATUS_Y                 20
 #define NEW_MODE_STATUS_RIGHT_MARGIN      10
 #define NEW_MODE_STATUS_GAP               6
-#define NEW_MODE_TITLE_Y                  18
+#define NEW_MODE_TITLE_Y                  15
 #define NEW_MODE_TITLE_W                  120
 #define NEW_MODE_TITLE_H                  36
 
@@ -50,7 +50,7 @@ extern u8 g_new_heat_proto_mode;
 #define NEW_MODE_PANEL_W                  280
 #define NEW_MODE_PANEL_H                  188
 
-#define NEW_MODE_ROW_FIRST_Y              68
+#define NEW_MODE_ROW_FIRST_Y              73
 #define NEW_MODE_ROW_GAP                  42
 #define NEW_MODE_ICON_X                   36
 #define NEW_MODE_LABEL_GAP                12
