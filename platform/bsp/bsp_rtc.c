673153fcb323a831eddc0693a5ae653a0c16c377
正心 <13217265055@163.com>
2026-07-08 11:10:24 +0800

    默认时间修改

--- platform/bsp/bsp_rtc.c
+++ platform/bsp/bsp_rtc.c
@@ -35,8 +35,8 @@ void rtc_clock_init(void)
 {
     tm_t tm;
     tm.year = 2026;
-    tm.mon  = 6;
-    tm.day = 29;
+    tm.mon  = 7;
+    tm.day = 8;
     tm.hour = 12;
     tm.min  = 0;
     tm.sec  = 0;
